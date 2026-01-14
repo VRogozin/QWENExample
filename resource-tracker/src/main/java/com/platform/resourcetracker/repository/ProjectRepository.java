2d9563f7e5ddd8fea5643c1bf59f82162420115f
VRogozin <vrogozin@gmail.com>
2026-01-14 20:24:40 +0300

    Delete resource-tracker directory

--- resource-tracker/src/main/java/com/platform/resourcetracker/repository/ProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.platform.resourcetracker.repository;
-
-import com.platform.resourcetracker.entity.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-}
